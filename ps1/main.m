6c8f21c4037e8802e2a4005143cc194cd518c175
Hong Dai Thanh <u0906962@soctf-imac-003.comp.nus.edu.sg>
2012-01-17 11:00:03 +0800

    Initial Commit

---
+++ ps1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ps1
+//
+//  Created by  on 1/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
